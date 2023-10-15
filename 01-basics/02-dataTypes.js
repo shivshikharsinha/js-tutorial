cb8cc979164ee9790bcc3d60277861f672c8c85c
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-15 07:27:02 +0000

    datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer one
+// alert("Warning!!") // we are using node.js
+
+console.log(6*10+9)
+
+console.log("Shiv")
+
+let name = "Shiv" // String
+let age = 22 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+/* 
+number => 2^53
+bigInt => big numbers
+string => "abc" / 'abc'
+boolean => true/false
+null => standalone value; datatype = object;
+undefined => value not defined; datatype = undefined
+symbol => to identify unique values
+
+
+object
+ */
+console.log("Type of Null - " + typeof null);
+console.log("Type of Undefined - " + typeof undefined);
