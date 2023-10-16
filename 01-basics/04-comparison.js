022c90a34bdca5e30f7bb0dc801eaccdedbb7f05
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-16 03:05:17 +0000

    comparison done

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1); 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //Unexpected outcome 
+
+// In javascript equality checks and comparison checks works differently. In the third line with Comparison operator (>=) the null is converted into 0 and then executed therefore ans is true. 
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+console.log("2"=== 2);
+// === : Strict Check - Checks data type and value
+// == : Checks only value
+
