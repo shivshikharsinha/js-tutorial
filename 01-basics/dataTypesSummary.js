03d4e96b3cfb8df266fd2b3cd21fb05b45d1d6da
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-16 04:00:03 +0000

    Stack and Heap Quick summary

--- 01-basics/dataTypesSummary.js
+++ 01-basics/dataTypesSummary.js
@@ -49,3 +49,30 @@ String	                                            "string"                  |
 Non - Primitive Types -                             "Object/ Object Function" |
 ------------------------------------------------------------------------------|
 */
+
+// ************************ STACK AND HEAP MEMORY *************************
+
+//  Stack (Primitive) , Heap (Non-Primitive) 
+ 
+let myName = "Shiv Shikhar Sinha"
+let anothername = "FLASHIV"; 
+anothername = "SSSinhaOfficial"
+console.log(anothername);
+console.log(myName);
+// A separate stack memory is allocated to store this value. 
+// Any changes to this does not affect the original myName variable. 
+// Changes reflect only in the copy of the variable.
+
+let UserDetails = {
+    name : "Shiv Shikhar Sinha",
+    userEmail: "sss@hotmail.com"
+}
+let UserDetailsDuplicate = UserDetails;
+
+UserDetailsDuplicate.userEmail = "sssinha@hotmail.com";
+console.log(UserDetails);
+console.log(UserDetailsDuplicate);
+
+// The data in the UserDetailsDuplicate is referenced to the same heap memory of the UserDetails.
+// So, even if we change the value of userEmail in the "UserDetailsDuplicate", the actual value (original one) is changed
+// and reflected when we console.log "UserDetails".
