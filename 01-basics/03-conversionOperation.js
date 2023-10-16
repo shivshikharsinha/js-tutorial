4a9ac9510e3a98eeea09c3264a6899019208e486
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-16 02:18:53 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* Conversion
+33 => 33 
+33AB => NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+/* Boolean Conversion
+1 => true; 0 => false
+"" => false
+"abcd" => true 
+ */
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
