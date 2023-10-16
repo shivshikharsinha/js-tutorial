9ed0ebe98a9e947fbeeb3796cd69fab6e3950fcb
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-16 02:48:13 +0000

    Operations on dataTypes

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -28,5 +28,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* OPERATIONS *******************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+/* 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(2/4);
+console.log(2%4);
+*/
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
