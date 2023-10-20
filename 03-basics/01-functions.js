d61fac4a4dc52813682cffae588a38e51e8ac687
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-20 03:41:53 +0000

    function with parameters

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -27,12 +27,41 @@ const result = addTwoNumbers(3, 5)
 
 function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("PLease enter a username");
+        console.log("Please enter a username");
         return
     }
-    return `${username} just logged in`
+    return `Hi ${username} Welcome.`
 }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "shiv",
+    prices: 2999
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
