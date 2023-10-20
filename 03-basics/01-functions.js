473664a7d92bda33b6cbbfcbb0501866505df43e
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-20 03:23:15 +0000

    functions basics

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,38 @@
+
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
