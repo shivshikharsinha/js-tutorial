dc1598f08dc2729011c73abcb13871c2636ff863
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2023-10-21 07:34:50 +0000

    scopes-2, this and arrow functions completed

--- 03-basics/02-scopes.js
+++ 03-basics/02-scopes.js
@@ -3,12 +3,53 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ Interesting ++++++++++++++++++
+
+//  Two ways of methods declaration
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
 }
 
 
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+addTwo(5) // throws error
+const addTwo = function(num){
+    return num + 2
+}
---
+++ 03-basics/03-arrow.js
@@ -0,0 +1,57 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// Explicit return using curly braces with rreturn' keyword.
+// Implicit return using parantheses without the 'return' keyword (used widely in react).
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
