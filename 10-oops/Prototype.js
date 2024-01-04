34b6f1ca094d12b2c946c2fd3cab3ce9227a65ba
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2024-01-04 20:42:10 +0000

    Added Prototype

--- 10-oops/Prototype.js
+++ 10-oops/Prototype.js
@@ -1,2 +1,15 @@
-let myName = "Shiv"
-console.log(myName.length);
+let myName = "Shiv      "
+
+console.log(myName.truelength);
+
+let myHeros = ["thor", "loki", "spiderman", "ironman"]
+
+let heroPowers = {
+    'thor' : "hammer",
+    'loki' : "mischief",
+    'spiderman': "web",
+    'ironman' : "technology",
+    getSpiderPower: function(){
+        console.log(`Spidey's Power is ${this.spiderman}`);
+    }
+}
