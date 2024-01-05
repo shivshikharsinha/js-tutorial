38a1b443a429c44b243b006a61e074a017dcd2eb
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2024-01-05 16:35:31 +0000

    Prototype Completed

--- 10-oops/Prototype.js
+++ 10-oops/Prototype.js
@@ -1,6 +1,6 @@
 let myName = "Shiv      "
 
-console.log(myName.truelength);
+// console.log(myName.truelength);
 
 let myHeros = ["thor", "loki", "spiderman", "ironman"]
 
@@ -13,3 +13,50 @@ let heroPowers = {
         console.log(`Spidey's Power is ${this.spiderman}`);
     }
 }
+
+Object.prototype.shiv= function(){
+    console.log("Shiv is present everwhere");
+}
+
+Array.prototype.heyShiv = function(){
+    console.log("Shiv says Hi");
+}
+// heroPowers.shiv();
+
+// myHeros.shiv();
+// myHeros.heyShiv();
+
+// heroPowers.heyShiv();
+
+// Inheritance
+
+const User = {
+    name: 'Shiv',
+    email: 'Google@gmail.com'
+}
+
+const Teacher = {
+    makeVideos :  true
+}
+const TeachingSupport = {
+    isAvailable : true
+}
+const TASupport = {
+    makeAssignment: "JS Assignment",
+    fullTime : true,
+    __proto__ : TeachingSupport
+}
+Teacher.__proto__ = User
+
+// Modern Syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "ChaiAurCode     "
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True Length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"iceTea".trueLength()
