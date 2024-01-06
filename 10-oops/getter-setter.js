83a6af475c16bc5e1afc2f97f2346679a4d1e3f6
Shiv Shikhar Sinha <64603451+shivshikharsinha@users.noreply.github.com>
2024-01-06 18:17:31 +0000

    Getters and Setters Done

---
+++ 10-oops/getter-setter.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(email, password){
+        this.email = email
+        this.password = password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+    get password() {
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value
+    }
+}
+
+const shiv = new User("sss@gmail.com", "123XYZ")
+console.log(shiv.email);
+console.log(shiv.password);
